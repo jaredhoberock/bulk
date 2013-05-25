8d755ee156f7a0976fe22f5bcfae002494184e9a
Jared Hoberock <jaredhoberock@gmail.com>
2013-05-24 17:27:54 -0700

    Add ping_pong example

---
+++ ping_pong.cu
@@ -0,0 +1,77 @@
+#include <thrust/device_vector.h>
+#include <bulk/bulk.hpp>
+#include <cstdio>
+
+struct ping
+{
+  __device__
+  void operator()(volatile int *ball)
+  {
+    if(threadIdx.x == 0)
+    {
+      *ball = 1;
+
+      for(unsigned int next_state = 2;
+          next_state < 25;
+          next_state += 2)
+      {
+        while(*ball != next_state)
+        {
+          printf("ping waiting for return\n");
+        }
+
+        *ball += 1;
+
+        printf("ping! ball is now %d\n", next_state);
+      }
+    }
+  }
+};
+
+struct pong
+{
+  __device__
+  void operator()(volatile int *ball)
+  {
+    if(threadIdx.x == 0)
+    {
+      for(unsigned int next_state = 1;
+          next_state < 25;
+          next_state += 2)
+      {
+        while(*ball != next_state)
+        {
+          printf("pong waiting for return\n");
+        }
+
+        *ball += 1;
+
+        printf("pong! ball is now %d\n", next_state);
+      }
+    }
+  }
+};
+
+int main()
+{
+  cudaStream_t s1, s2;
+  cudaStreamCreate(&s1);
+  cudaStreamCreate(&s2);
+
+  using bulk::par_async;
+  using bulk::async;
+
+  thrust::device_vector<int> ball(1);
+
+  bulk::future<void> t1 = async(par_async(s1, 1), ping(), thrust::raw_pointer_cast(&*ball.data()));
+  bulk::future<void> t2 = async(par_async(s2, 1), pong(), thrust::raw_pointer_cast(&*ball.data()));
+
+  t1.wait();
+  t2.wait();
+
+  cudaStreamDestroy(s1);
+  cudaStreamDestroy(s2);
+
+  return 0;
+}
+
