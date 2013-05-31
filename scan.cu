30259deecb2d9a25808530c2c208aecbb0370c52
Jared Hoberock <jaredhoberock@gmail.com>
2013-05-31 14:24:37 -0700

    Begin scan project

---
+++ scan.cu
@@ -0,0 +1,178 @@
+#include <moderngpu.cuh>
+#include <thrust/scan.h>
+#include <thrust/sequence.h>
+#include <thrust/device_vector.h>
+#include <thrust/functional.h>
+#include <thrust/random.h>
+#include <cassert>
+#include <iostream>
+#include "time_invocation_cuda.hpp"
+
+
+typedef int T;
+
+
+template<mgpu::MgpuScanType Type, typename InputIt, typename OutputIt, typename Op>
+void Scan(InputIt data_global, int count, OutputIt dest_global, Op op, typename Op::value_type* total, bool totalAtEnd, mgpu::CudaContext& context)
+{
+  typedef typename Op::value_type value_type;
+  typedef typename Op::result_type result_type;
+  
+  const int CutOff = 20000;
+
+  if(count < CutOff)
+  {
+    const int NT = 512;
+    const int VT = 3;
+    MGPU_MEM(value_type) totalDevice;
+    if(total) totalDevice = context.Malloc<value_type>(1);
+    
+    mgpu::KernelParallelScan<NT, VT, Type><<<1, NT, 0, context.Stream()>>>(
+    	data_global, count, op, total ? totalDevice->get() : (value_type*)0,
+    	totalAtEnd ? (dest_global + count) : (result_type*)0, dest_global);
+    
+    if(total)
+    {
+      totalDevice->ToHost(total, 1);
+    }
+  }
+  else
+  {
+    // Run the parallel raking reduce as an upsweep.
+    const int NT = 128;
+    const int VT = 7;
+    typedef mgpu::LaunchBoxVT<NT, VT> Tuning;
+    int2 launch = Tuning::GetLaunchParams(context);
+    const int NV = launch.x * launch.y;
+    
+    int numTiles = MGPU_DIV_UP(count, NV);
+    int numBlocks = std::min(context.NumSMs() * 25, numTiles);
+    int2 task = mgpu::DivideTaskRange(numTiles, numBlocks);
+    
+    MGPU_MEM(value_type) reductionDevice = context.Malloc<value_type>(numBlocks + 1);
+
+    value_type* totalDevice = total ? (reductionDevice->get() + numBlocks) : (value_type*)0;
+    	
+    mgpu::KernelReduce<Tuning><<<numBlocks, launch.x, 0, context.Stream()>>>(data_global, count, task, reductionDevice->get(), op);
+    
+    // Run a parallel latency-oriented scan to reduce the spine of the 
+    // raking reduction.
+    const int NT2 = 256;
+    const int VT2 = 3;
+    mgpu::KernelParallelScan<NT2, VT2, mgpu::MgpuScanTypeExc><<<1, NT2, 0, context.Stream()>>>(reductionDevice->get(), numBlocks, mgpu::ScanOpValue<Op>(op), totalDevice, (value_type*)0, reductionDevice->get());
+    
+    if(total)
+    {
+      cudaMemcpy(total, totalDevice, sizeof(value_type), cudaMemcpyDeviceToHost);
+    }
+    
+    // Run a raking scan as a downsweep.
+    mgpu::KernelScanDownsweep<Tuning, Type><<<numBlocks, launch.x, 0, context.Stream()>>>(data_global, count, task, reductionDevice->get(), dest_global, totalAtEnd, op);
+  }
+}
+
+
+template<typename InputIterator, typename OutputIterator>
+OutputIterator my_inclusive_scan(InputIterator first, InputIterator last, OutputIterator result)
+{
+  mgpu::ContextPtr ctx = mgpu::CreateCudaDevice(0);
+
+  mgpu::Scan<mgpu::MgpuScanTypeInc>(thrust::raw_pointer_cast(&*first),
+                                    last - first,
+                                    thrust::raw_pointer_cast(&*result),
+                                    mgpu::ScanOp<mgpu::ScanOpTypeAdd,int>(),
+                                    (int*)0,
+                                    false,
+                                    *ctx);
+
+  return result + (last - first);
+}
+
+
+void my_scan(thrust::device_vector<T> *data)
+{
+  my_inclusive_scan(data->begin(), data->end(), data->begin());
+}
+
+
+void do_it(size_t n)
+{
+  thrust::host_vector<T> h_input(n);
+  thrust::sequence(h_input.begin(), h_input.end());
+
+  thrust::host_vector<T> h_result(n);
+
+  thrust::inclusive_scan(h_input.begin(), h_input.end(), h_result.begin());
+
+  thrust::device_vector<T> d_input = h_input;
+  thrust::device_vector<T> d_result(d_input.size());
+
+  my_inclusive_scan(d_input.begin(), d_input.end(), d_result.begin());
+
+  cudaError_t error = cudaGetLastError();
+
+  if(error)
+  {
+    std::cerr << "CUDA error: " << cudaGetErrorString(error) << std::endl;
+  }
+
+  assert(h_result == d_result);
+}
+
+
+template<typename InputIterator, typename OutputIterator>
+OutputIterator mgpu_inclusive_scan(InputIterator first, InputIterator last, OutputIterator result)
+{
+  mgpu::ContextPtr ctx = mgpu::CreateCudaDevice(0);
+
+  mgpu::Scan<mgpu::MgpuScanTypeInc>(thrust::raw_pointer_cast(&*first),
+                                    last - first,
+                                    thrust::raw_pointer_cast(&*result),
+                                    mgpu::ScanOp<mgpu::ScanOpTypeAdd,int>(),
+                                    (int*)0,
+                                    false,
+                                    *ctx);
+
+  return result + (last - first);
+}
+
+
+void sean_scan(thrust::device_vector<T> *data)
+{
+  mgpu_inclusive_scan(data->begin(), data->end(), data->begin());
+}
+
+
+int main()
+{
+  for(size_t n = 1; n <= 1 << 20; n <<= 1)
+  {
+    std::cout << "Testing n = " << n << std::endl;
+    do_it(n);
+  }
+
+  thrust::default_random_engine rng;
+  for(int i = 0; i < 20; ++i)
+  {
+    size_t n = rng() % (1 << 20);
+   
+    std::cout << "Testing n = " << n << std::endl;
+    do_it(n);
+  }
+
+  thrust::device_vector<T> vec(1 << 24);
+
+  sean_scan(&vec);
+  double sean_msecs = time_invocation_cuda(20, sean_scan, &vec);
+
+  my_scan(&vec);
+  double my_msecs = time_invocation_cuda(20, my_scan, &vec);
+
+  std::cout << "Sean's time: " << sean_msecs << " ms" << std::endl;
+  std::cout << "My time: " << my_msecs << " ms" << std::endl;
+
+  std::cout << "My relative performance: " << sean_msecs / my_msecs << std::endl;
+
+  return 0;
+}
+
