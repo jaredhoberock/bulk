a21e71b006e36faf07d8c6857ff4155ebb85ded5
Jared Hoberock <jaredhoberock@gmail.com>
2013-06-06 22:32:53 -0700

    Optimize bulk::reduce

--- bulk/algorithm/reduce.hpp
+++ bulk/algorithm/reduce.hpp
@@ -6,71 +6,131 @@
 
 namespace bulk
 {
-
-
-template<typename ThreadGroup,
-         typename RandomAccessIterator,
-         typename T,
-         typename BinaryOperation>
-__device__
-T reduce(ThreadGroup &exec,
-         RandomAccessIterator first,
-         RandomAccessIterator last,
-         T init,
-         BinaryOperation binary_op)
+namespace detail
 {
-  T *s_sums = static_cast<int*>(bulk::malloc(exec, sizeof(T) * exec.size()));
+namespace reduce_detail
+{
 
-  unsigned int i = exec.this_thread.index();
-  
-  typename thrust::iterator_difference<RandomAccessIterator>::type n = last - first;
 
-  if(i < n)
-  {
-    T this_sum = first[i];
+template<typename ThreadGroup, typename RandomAccessIterator, typename Size, typename T, typename BinaryFunction>
+__device__ T destructive_reduce_n(ThreadGroup &g, RandomAccessIterator s_sums, Size n, T init, BinaryFunction binary_op)
+{
+  typedef int size_type;
 
-    for(i += exec.size();
-        i < n;
-        i += exec.size())
-    {
-      this_sum = binary_op(this_sum, first[i]);
-    } // end for i
+  size_type tid = g.this_thread.index();
 
-    // XXX this should be a placement new
-    s_sums[exec.this_thread.index()] = this_sum;
-  } // end if
-
-  exec.wait();
-
-  typedef typename ThreadGroup::size_type size_type;
-  size_type m = thrust::min<size_type>(n, exec.size());
+  Size m = n;
 
   while(m > 1)
   {
-    size_type half_m = m >> 1;
+    Size half_m = m >> 1;
 
-    if(exec.this_thread.index() < half_m)
+    if(tid < half_m)
     {
-      T old_val = s_sums[exec.this_thread.index()];
+      T old_val = s_sums[tid];
 
-      s_sums[exec.this_thread.index()] = binary_op(old_val, s_sums[m - exec.this_thread.index() - 1]);
+      s_sums[tid] = binary_op(old_val, s_sums[m - tid - 1]);
     } // end if
 
-    exec.wait();
+    g.wait();
 
     m -= half_m;
   } // end while
 
-  exec.wait();
+  g.wait();
 
   T result = (n > 0) ? binary_op(init,s_sums[0]) : init;
 
-  exec.wait();
-
-  bulk::free(exec, s_sums);
+  g.wait();
 
   return result;
-} // end reduce()
+}
+
+
+} // end reduce_detail
+} // end detail
+
+
+template<std::size_t groupsize, std::size_t grainsize, typename RandomAccessIterator, typename T, typename BinaryFunction>
+__device__
+T reduce(bulk::static_thread_group<groupsize,grainsize> &g,
+         RandomAccessIterator first,
+         RandomAccessIterator last,
+         T init,
+         BinaryFunction binary_op)
+{
+  typedef typename thrust::iterator_value<RandomAccessIterator>::type value_type;
+
+  typedef int size_type;
+
+  const size_type elements_per_group = groupsize * grainsize;
+
+  size_type tid = g.this_thread.index();
+
+  value_type this_sum;
+
+  bool this_sum_defined = false;
+
+  typename thrust::iterator_difference<RandomAccessIterator>::type n = last - first;
+
+  T *buffer = reinterpret_cast<value_type*>(bulk::malloc(g, groupsize * sizeof(T)));
+  
+  for(; first < last; first += elements_per_group)
+  {
+    size_type partition_size = thrust::min<size_type>(elements_per_group, last - first);
+    
+    // load input into register
+    value_type inputs[grainsize];
+
+    if(partition_size >= elements_per_group)
+    {
+      for(size_type i = 0; i < grainsize; ++i)
+      {
+        inputs[i] = first[groupsize * i + tid];
+      } // end for
+    } // end if
+    else
+    {
+      for(size_type i = 0; i < grainsize; ++i)
+      {
+        size_type index = groupsize * i + tid;
+        if(index < partition_size)
+        {
+          inputs[i] = first[index];
+        } // end if
+      } // end for
+    } // end else
+    
+    // sum sequentially
+    for(size_type i = 0; i < grainsize; ++i)
+    {
+      size_type index = groupsize * i + g.this_thread.index();
+      if(index < partition_size)
+      {
+        value_type x = inputs[i];
+        this_sum = (i || this_sum_defined) ? binary_op(this_sum, x) : x;
+      } // end if
+    } // end for
+
+    this_sum_defined = true;
+  } // end for
+
+  if(this_sum_defined)
+  {
+    buffer[tid] = this_sum;
+  } // end if
+
+  g.wait();
+
+  // reduce across the block
+  T result = detail::reduce_detail::destructive_reduce_n(g, buffer, thrust::min<size_type>(groupsize,n), init, binary_op);
+
+  bulk::free(g,buffer);
+
+  g.wait();
+
+  return result;
+} // end reduce
 
 
 } // end bulk
