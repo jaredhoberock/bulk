482768d7e5a55e3a8c697995e46ed6bea20d516f
Jared Hoberock <jaredhoberock@gmail.com>
2013-10-11 14:43:36 -0700

    Require alignment of block.
    Fixes #32

--- bulk/malloc.hpp
+++ bulk/malloc.hpp
@@ -18,6 +18,7 @@
 
 #include <bulk/detail/config.hpp>
 #include <bulk/detail/pointer_traits.hpp>
+#include <bulk/detail/alignment.hpp>
 #include <bulk/uninitialized.hpp>
 #include <thrust/detail/config.h>
 #include <cstdlib>
@@ -183,7 +184,8 @@ class singleton_unsafe_on_chip_allocator
 
 
   private:
-    class block
+    // align to two words
+    class block : public bulk::detail::aligned_type<sizeof(size_t) + sizeof(block*)>
     {
       public:
         __device__ inline size_t size() const
